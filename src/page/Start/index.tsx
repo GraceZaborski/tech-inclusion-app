afe559c63f52ba28444d37fc2f908164ec4b8d0b
GraceZaborski <grace.zaborski@beamery.com>
2022-05-31 09:21:00 +0100

    style: MUI

--- src/page/Start/index.tsx
+++ src/page/Start/index.tsx
@@ -1,6 +1,9 @@
+import Button from "@mui/material/Button";
+
 function Start() {
   return (
     <div>
+      <Button variant="contained">Hello World</Button>
       <div>Start Here</div>
     </div>
   );
